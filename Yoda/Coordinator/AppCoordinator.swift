57191014ec40ae6d8a0e3064c21669c462520b1e
Chris Myers <cmyers78@gmail.com>
2021-10-11 14:10:04 -0400

    initial commit

---
+++ Yoda/Coordinator/AppCoordinator.swift
@@ -0,0 +1,16 @@
+//
+//  AppCoordinator.swift
+//  Yoda
+//
+//  Created by Chris Myers on 10/11/21.
+//
+
+import Foundation
+
+class AppCoordinator: Coordinator {
+	
+	
+	func start() {
+		print("Start Main VC or child coordinator")
+	}
+}
---
+++ Yoda/Coordinator/Coordinator.swift
@@ -0,0 +1,12 @@
+//
+//  Coordinator.swift
+//  Yoda
+//
+//  Created by Chris Myers on 10/11/21.
+//
+
+import Foundation
+
+protocol Coordinator {
+	func start()
+}
